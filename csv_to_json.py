a266ed2dd587dafdc2a421c35f809d30c516f6b5
ngoctra123 <ngoctra281092@gmail.com>
2026-03-08 21:09:50 +0700

    Create csv_to_json.py

---
+++ csv_to_json.py
@@ -0,0 +1,14 @@
+import csv
+import json
+
+def convert_csv_json(csv_path, json_path):
+    data = []
+    with open(csv_path, encoding='utf-8') as csvf:
+        csv_reader = csv.DictReader(csvf)
+        for rows in csv_reader:
+            data.append(rows)
+            
+    with open(json_path, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+# convert_csv_json('data.csv', 'data.json')
