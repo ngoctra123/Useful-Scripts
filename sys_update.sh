de27f962785430f5dcd0b1facb080f93829f8fab
ngoctra123 <ngoctra281092@gmail.com>
2026-03-09 14:13:53 +0700

    Update sys_update.sh

--- sys_update.sh
+++ sys_update.sh
@@ -4,3 +4,8 @@ sudo apt update && sudo apt upgrade -y
 echo "Dọn dẹp các file thừa..."
 sudo apt autoremove -y
 echo "Hoàn thành!"
+#!/bin/bash
+# Dòng trên gọi là Shebang, thông báo cho hệ thống rằng file này sẽ được thực thi bằng Bash shell
+
+# In thông báo ra màn hình để người dùng biết quá trình bắt đầu
+echo "Đang bắt đầu cập nhật hệ thống..."
