8dcbf05b1d6314c31972844049cf3c9a1dddd210
ngoctra123 <ngoctra281092@gmail.com>
2026-03-09 10:54:25 +0700

    Update csv_to_json.py

--- csv_to_json.py
+++ csv_to_json.py
@@ -21,3 +21,15 @@ def convert_csv_json(csv_path, json_path):
     json_path: Đường dẫn tệp JSON đích sẽ tạo ra.
     """
     data = [] # Khởi tạo danh sách để chứa dữ liệu từ các dòng của CSV
+  # Bước 1: Mở và đọc file CSV
+    # Sử dụng 'with' để đảm bảo file được đóng tự động sau khi xử lý xong
+    # encoding='utf-8' giúp đọc được các ký tự tiếng Việt hoặc ký tự đặc biệt
+    with open(csv_path, encoding='utf-8') as csvf:
+        # DictReader sẽ biến mỗi dòng trong CSV thành một Dictionary (Object)
+        # Trong đó: Key là tiêu đề cột, Value là dữ liệu tương ứng của dòng đó
+        csv_reader = csv.DictReader(csvf)
+        
+        # Duyệt qua từng dòng dữ liệu trong file CSV
+        for rows in csv_reader:
+            # Thêm dictionary của dòng hiện tại vào danh sách data
+            data.append(rows)
